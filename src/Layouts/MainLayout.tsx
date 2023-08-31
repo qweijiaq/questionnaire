aa0c9a6ad696e3c12cb9fbb8c8d79c01878b620c
WeiJia <2112025700@qq.com>
2023-08-31 12:50:50 +0800

    feat: 引入 Redux 做状态管理

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Layout } from 'antd'
+import { Layout, Spin } from 'antd'
 import styles from './MainLayout.module.scss'
 import Logo from '../components/Logo'
 import UserInfo from '../components/UserInfo'
+import useLoadUserData from '../hooks/useLoadUserData'
+import useNavPage from '../hooks/useNavPage'
 
 const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
+  const { waitingUserData } = useLoadUserData()
+  useNavPage(waitingUserData)
   return (
     <Layout>
       <Header className={styles.header}>
@@ -19,7 +23,13 @@ const MainLayout: FC = () => {
         </div>
       </Header>
       <Content className={styles.content}>
-        <Outlet></Outlet>
+        {waitingUserData ? (
+          <div style={{ textAlign: 'center', marginTop: '60px' }}>
+            <Spin />
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </Content>
       <Footer className={styles.footer}>蜗牛问卷 &copy;2023 - present. Created by Pony Wei</Footer>
     </Layout>
--- src/Layouts/QuestionLayout.tsx
+++ src/Layouts/QuestionLayout.tsx
@@ -1,12 +1,24 @@
+import { Spin } from 'antd'
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import useLoadUserData from '../hooks/useLoadUserData'
+import useNavPage from '../hooks/useNavPage'
 
 const QuestionLayout: FC = () => {
+  const { waitingUserData } = useLoadUserData()
+  useNavPage(waitingUserData)
+
   return (
     <>
-      <div>QuestionLayout</div>
+      <p>QuestionLayout</p>
       <div>
-        <Outlet />
+        {waitingUserData ? (
+          <div style={{ textAlign: 'center', marginTop: '60px' }}>
+            <Spin />
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </div>
     </>
   )
